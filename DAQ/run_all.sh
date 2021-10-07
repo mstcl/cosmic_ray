f57807ad1a5b631aa2f80c8810e222a028971a0d
Jim B <jim.brooke@cern.ch>
2021-10-07 09:53:36 +0100

    Update run_all.sh

--- DAQ/run_all.sh
+++ DAQ/run_all.sh
@@ -5,9 +5,9 @@
 #  bash run_all.sh
 #
 
-acquire.py -t 100 -o thresh100.txt -0 100 -1 100 -2 100 -3 100
-acquire.py -t 100 -o thresh200.txt -0 200 -1 200 -2 200 -3 200
-acquire.py -t 100 -o thresh300.txt -0 300 -1 300 -2 300 -3 300
-acquire.py -t 100 -o thresh400.txt -0 400 -1 400 -2 400 -3 400
-acquire.py -t 100 -o thresh500.txt -0 500 -1 500 -2 500 -3 500
-acquire.py -t 100 -o thresh600.txt -0 600 -1 600 -2 600 -3 600
+acquire.py -t 100 -o thresh100.txt -0 100 -1 100 -2 100 -3 100 -e 0x1
+acquire.py -t 100 -o thresh200.txt -0 200 -1 200 -2 200 -3 200 -e 0x1
+acquire.py -t 100 -o thresh300.txt -0 300 -1 300 -2 300 -3 300 -e 0x1
+acquire.py -t 100 -o thresh400.txt -0 400 -1 400 -2 400 -3 400 -e 0x1
+acquire.py -t 100 -o thresh500.txt -0 500 -1 500 -2 500 -3 500 -e 0x1
+acquire.py -t 100 -o thresh600.txt -0 600 -1 600 -2 600 -3 600 -e 0x1
