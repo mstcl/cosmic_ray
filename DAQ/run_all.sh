d0cd95c6b819cdd40af69d7d41357fd370a51678
Nhat <lckdscl+github@protonmail.com>
2023-01-26 15:50:52 +0000

    Edit: batch test all channels, one at a time, from 100 to 1000 mV in steps of 100 mV

--- DAQ/run_all.sh
+++ DAQ/run_all.sh
@@ -4,10 +4,18 @@
 # Usage :
 #  bash run_all.sh
 #
-
-acquire.py -t 100 -o thresh100.txt -0 100 -1 100 -2 100 -3 100 -e 0x1
-acquire.py -t 100 -o thresh200.txt -0 200 -1 200 -2 200 -3 200 -e 0x1
-acquire.py -t 100 -o thresh300.txt -0 300 -1 300 -2 300 -3 300 -e 0x1
-acquire.py -t 100 -o thresh400.txt -0 400 -1 400 -2 400 -3 400 -e 0x1
-acquire.py -t 100 -o thresh500.txt -0 500 -1 500 -2 500 -3 500 -e 0x1
-acquire.py -t 100 -o thresh600.txt -0 600 -1 600 -2 600 -3 600 -e 0x1
+i=0
+while [ $i -ne 4 ]
+do
+    acquire.py -t 100 -o thresh100_${i}.txt -0 100 -e 0x$(dc -e "2 ${i} ^ p")
+    acquire.py -t 100 -o thresh200_${i}.txt -0 200 -e 0x$(dc -e "2 ${i} ^ p")
+    acquire.py -t 100 -o thresh300_${i}.txt -0 300 -e 0x$(dc -e "2 ${i} ^ p")
+    acquire.py -t 100 -o thresh400_${i}.txt -0 400 -e 0x$(dc -e "2 ${i} ^ p")
+    acquire.py -t 100 -o thresh500_${i}.txt -0 500 -e 0x$(dc -e "2 ${i} ^ p")
+    acquire.py -t 100 -o thresh600_${i}.txt -0 600 -e 0x$(dc -e "2 ${i} ^ p")
+    acquire.py -t 100 -o thresh700_${i}.txt -0 700 -e 0x$(dc -e "2 ${i} ^ p")
+    acquire.py -t 100 -o thresh800_${i}.txt -0 800 -e 0x$(dc -e "2 ${i} ^ p")
+    acquire.py -t 100 -o thresh900_${i}.txt -0 900 -e 0x$(dc -e "2 ${i} ^ p")
+    acquire.py -t 100 -o thresh1000_${i}.txt -0 1000 -e 0x$(dc -e "2 ${i} ^ p")
+    i=$(($i+1))
+done
