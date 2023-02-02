b2fce19ba4f046d18c0861654983e6dd713640ba
Cris Paunica <ur20316@it055170.users.bris.ac.uk>
2023-02-02 10:34:47 +0000

    fixed bugs

--- DAQ/run_all.sh
+++ DAQ/run_all.sh
@@ -4,13 +4,13 @@
 # Usage :
 #  bash run_all.sh
 #
-i=0
-n=50
+i=1
 while [ $i -ne 4 ]
 do
+    n=50
     while [ $n -ne 1010 ]
     do
-        acquire.py -t 100 -o thresh100_${i}.txt -0 ${n} -e 0x$(dc -e "2 ${i} ^ p")
+        acquire.py -t 100 -o thresh${n}_${i}.txt -0 ${n} -e 0x$(dc -e "2 ${i} ^ p")
         n=$(($n+10))
     done
     i=$(($i+1))
