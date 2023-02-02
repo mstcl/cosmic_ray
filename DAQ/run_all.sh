830aeebf072aef0840b62d670be009f0e2f52974
Nhat <lckdscl+github@protonmail.com>
2023-02-02 17:00:05 +0000

    Edit: third run values

--- DAQ/run_all.sh
+++ DAQ/run_all.sh
@@ -4,14 +4,14 @@
 # Usage :
 #  bash run_all.sh
 #
-i=1
+i=0
 while [ $i -ne 4 ]
 do
-    n=50
-    while [ $n -ne 1010 ]
+    n=10
+    while [ $n -ne 2010 ]
     do
         acquire.py -t 100 -o thresh${n}_${i}.txt -0 ${n} -e 0x$(dc -e "2 ${i} ^ p")
-        n=$(($n+10))
+        n=$(($n+50))
     done
     i=$(($i+1))
 done
