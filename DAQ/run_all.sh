3e2c8ab972852d10ba993d940acd669530707ca0
Nhat <lckdscl+github@protonmail.com>
2023-02-09 12:56:50 +0000

    Fixed: other channels other than channel 0 actually works now

--- DAQ/run_all.sh
+++ DAQ/run_all.sh
@@ -10,7 +10,7 @@ do
     n=50
     while [ $n -le 1000 ]
     do
-        acquire.py -t 100 -o thresh${n}_${i}.txt -0 ${n} -e 0x$(dc -e "2 ${i} ^ p")
+        acquire.py -t 100 -o thresh${n}_${i}.txt -${i} ${n} -e 0x$(dc -e "2 ${i} ^ p")
         n=$(($n+100))
     done
     i=$(($i+1))
