e8bf96155a8ece42c08a07b310856bca748da7ca
Cris Paunica <ur20316@it055170.users.bris.ac.uk>
2023-02-09 12:39:01 +0000

    fix not equal condition to less equal

--- DAQ/run_all.sh
+++ DAQ/run_all.sh
@@ -4,14 +4,14 @@
 # Usage :
 #  bash run_all.sh
 #
-i=0
-while [ $i -ne 4 ]
+i=1
+while [ $i -le 2 ]
 do
-    n=10
-    while [ $n -ne 2010 ]
+    n=50
+    while [ $n -le 1000 ]
     do
         acquire.py -t 100 -o thresh${n}_${i}.txt -0 ${n} -e 0x$(dc -e "2 ${i} ^ p")
-        n=$(($n+50))
+        n=$(($n+100))
     done
     i=$(($i+1))
 done
