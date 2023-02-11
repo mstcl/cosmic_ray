64f55a54665d240cc12d415d591ca6a5fe86498a
Cris Paunica <ur20316@it055170.users.bris.ac.uk>
2023-02-11 17:35:53 +0000

    added path variable in data for ease of location choice

--- analysis/batch_convert.sh
+++ analysis/batch_convert.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-for filename in $HOME/data/*.txt
+for filename in $HOME/data/${1}/*.txt
   do
     [ -e "$filename" ] || continue
     convert.py -i "$filename" -o "$(echo "${filename}" | cut -d "." -f 1).dat"
