04c32c294eb0c982f1d668ac58c64756bff81e1b
Cris Paunica <ur20316@it055170.users.bris.ac.uk>
2023-02-11 17:34:30 +0000

    added variables for ease of edits

--- DAQ/run_all.sh
+++ DAQ/run_all.sh
@@ -4,14 +4,18 @@
 # Usage :
 #  bash run_all.sh
 #
-i=1
-while [ $i -le 2 ]
+i=0 #current channel (starts at initial channel)
+minv=10 #starting threshold value
+maxv=2000 #final threshold value
+f=3 #final channel
+increment=10 #increment for threshold value
+while [ $i -le $f ]
 do
-    n=50
-    while [ $n -le 1000 ]
+    n=$minv #starting value
+    while [ $n -le $maxv ]
     do
         acquire.py -t 100 -o thresh${n}_${i}.txt -${i} ${n} -e 0x$(dc -e "2 ${i} ^ p")
-        n=$(($n+100))
+        n=$(($n+$increment))
     done
     i=$(($i+1))
 done
