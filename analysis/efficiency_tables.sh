1a8fbe581019dba7fe6abf04dccd4523d294770c
lckdscl <33115327+mstcl@users.noreply.github.com>
2023-03-02 11:47:58 +0000

    Edit: run the correct file

--- analysis/efficiency_tables.sh
+++ analysis/efficiency_tables.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-touch C0-eff.txt
-touch C1-eff.txt
-touch C2-eff.txt
-touch C3-eff.txt
+touch C0-eff-sum.txt
+touch C1-eff-sum.txt
+touch C2-eff-sum.txt
+touch C3-eff-sum.txt
 
 for filename in $HOME/data/T1458-C1/*.dat
     do
         #note i have no idea what this command does, but i assume it makes sure the current file is not being considered
         [ -e "$filename" ] || continue
 	echo Analysing ${!filename}
-	analysis.py -i "$filename" 
+	analysis_triple_coincidence_sum.py -i "$filename" 
     
 done    
